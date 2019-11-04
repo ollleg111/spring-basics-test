f2ede7db8f17e7b6b400427fc512ab14597285e1
oleg.maliy14@gmail.com <q2301299266>
2019-11-04 10:30:02 +0200

    -0- 24/11/2019

--- src/main/java/com/lesson3/hw3/file_storage/controller/StorageController.java
+++ src/main/java/com/lesson3/hw3/file_storage/controller/StorageController.java
@@ -3,6 +3,7 @@ package com.lesson3.hw3.file_storage.controller;
 import com.fasterxml.jackson.core.JsonParseException;
 import com.fasterxml.jackson.databind.JsonMappingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.type.MapType;
 import com.lesson3.hw3.file_storage.model.File;
 import com.lesson3.hw3.file_storage.model.Storage;
 import com.lesson3.hw3.file_storage.service.FileService;
@@ -10,10 +11,15 @@ import com.lesson3.hw3.file_storage.service.StorageService;
 import org.hibernate.HibernateException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 import java.io.IOException;
 import java.io.InputStream;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
 
 @Controller
 @RequestMapping("/storage")
@@ -148,16 +154,81 @@ public class StorageController {
         }
     }
 
-    @PutMapping("/transfer/All")
-    public void transferAll(@RequestParam Storage storageFrom,
-                            @RequestParam Storage storageTo) throws HibernateException {
-        storageService.transferAll(storageFrom, storageTo);
+//    ObjectMapper mapper = new ObjectMapper();
+//    MapType type = mapper.getTypeFactory().constructMapType(
+//            Map.class, String.class, Object.class);
+//    Map<String, Object> data = mapper.readValue(jsonString, type);
+
+//    @PutMapping("/transfer/All")
+//    public void transferAll(@RequestParam Storage storageFrom,
+//                            @RequestParam Storage storageTo) throws HibernateException {
+//        storageService.transferAll(storageFrom, storageTo);
+//    }
+//
+//    @PutMapping("/transfer/{id}")
+//    public void transferFile(@RequestParam Storage storageFrom,
+//                             @RequestParam Storage storageTo,
+//                             @PathVariable long id) throws HibernateException {
+//        storageService.transferFile(storageFrom, storageTo, id);
+//    }
+
+    @RequestMapping(method = RequestMethod.PUT,
+            value = "/transferAll",
+            produces = "text/plan")
+    public @ResponseBody
+    String transferAll(InputStream data) throws HibernateException {
+        try {
+            ObjectMapper mapper = new ObjectMapper();
+            MapType type = mapper.getTypeFactory().constructMapType(Map.class, String.class, Storage.class);
+            Map<String, Storage> dataMap = mapper.readValue(data, type);
+
+            List<Storage> arrayList = new ArrayList<>();
+            for (Map.Entry<String, Storage> entry : dataMap.entrySet()) {
+                arrayList.add(entry.getValue());
+            }
+            storageService.transferAll(arrayList.get(0), arrayList.get(1));
+
+            return "All from storage with id: " + arrayList.get(0).getId() +
+                    " was transferred to storage with id: " + arrayList.get(1).getId();
+
+        } catch (JsonParseException e) {
+            return e.getMessage();
+        } catch (JsonMappingException e) {
+            return e.getMessage();
+        } catch (IOException e) {
+            return e.getMessage();
+        }
     }
 
-    @PutMapping("/transfer/{id}")
-    public void transferFile(@RequestParam Storage storageFrom,
-                             @RequestParam Storage storageTo,
-                             @PathVariable long id) throws HibernateException {
-        storageService.transferFile(storageFrom, storageTo, id);
+    @RequestMapping(method = RequestMethod.PUT,
+            value = "/transferFile",
+            produces = "text/plan")
+    public @ResponseBody
+    String transferFile(InputStream data) throws HibernateException {
+        try {
+            ObjectMapper mapper = new ObjectMapper();
+            MapType type = mapper.getTypeFactory().constructMapType(Map.class, String.class, Object.class);
+            Map<String, Object> dataMap = mapper.readValue(data, type);
+
+            List<Object> arrayList = new ArrayList<>();
+            for (Map.Entry<String, Object> entry : dataMap.entrySet()) {
+                arrayList.add(entry.getValue());
+            }
+            Storage storageFrom = (Storage) arrayList.get(0);
+            Storage storageTo = (Storage) arrayList.get(1);
+            Long id = (Long) arrayList.get(2);
+
+            storageService.transferFile(storageFrom, storageTo, id);
+
+            return "File with id: " + id + " moved from storage with id: " + storageFrom.getId() +
+                    " was transferred to storage with id: " + storageTo.getId();
+
+        } catch (JsonParseException e) {
+            return e.getMessage();
+        } catch (JsonMappingException e) {
+            return e.getMessage();
+        } catch (IOException e) {
+            return e.getMessage();
+        }
     }
 }
